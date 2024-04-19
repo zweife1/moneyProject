adae1ca60d0957ad092310d22b7329f2f111630a
Diego-Jaimes <131845495+Diego-Jaimes@users.noreply.github.com>
2024-04-18 19:46:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Hello Diego, sorry I messed up the other one.
 # I think I know how to use git now tho.
 #testing git pull and git fetch
+#testing git pull and git fetch pt2
 import numpy
 import pandas
 import matplotlib
