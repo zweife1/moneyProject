78ca96de172852948729b88749c6c23286cf304b
Diego-Jaimes <131845495+Diego-Jaimes@users.noreply.github.com>
2024-04-18 19:44:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Hello Diego, sorry I messed up the other one.
 # I think I know how to use git now tho.
+#testing git pull and git fetch
 import numpy
 import pandas
 import matplotlib
 
 print("que guey")
 print("\nello wurld")
+
